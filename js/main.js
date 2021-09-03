9e26a0a468836706adba7601aa56cc83899c4b0e
Daniel <lekarev8888@mail.ru>
2021-09-03 21:10:32 +0300

    поправил слайдер

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(function() {
     $('.slider__box').slick({
         prevArrow: '<img src="images/arrow_left.svg" alt="alt" class="slider__arrow arrow-left">',
-        nextArrow: '<img src="images/arrow_right.svg" alt="alt" class="slider__arrow arrow-right">'
+        nextArrow: '<img src="images/arrow_right.svg" alt="alt" class="slider__arrow arrow-right">',
+        responsive: [{
+            breakpoint: 481,
+            settings: {
+                arrows: false
+            }
+        }]
     });
 
     $('.menu_btn').on('click', function() {
